13bc93fff4969a41e680fee9a2c4a2449dd39f85
Shaila Zaman <shaila.zaman96@gmail.com>
2020-11-03 23:18:01 -0600

    refactored code

--- scripts/7.dc-generate-meta-data-break-activity.R
+++ scripts/7.dc-generate-meta-data-break-activity.R
@@ -2,6 +2,7 @@
 #--------LIBRARIES--------#
 #-------------------------#
 library(dplyr)
+library(zoo)
 
 
 
@@ -9,7 +10,11 @@ library(dplyr)
 #-------------------------#
 #-----GLOBAL VARIABLES----#
 #-------------------------#
-source(file.path(script_dir, 'us-common-functions.R'))
+# script_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+# project_dir <- dirname(script_dir)
+# setwd(project_dir)
+# 
+# source(file.path(script_dir, 'us-common-functions.R'))
 
 
 
