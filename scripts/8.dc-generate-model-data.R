9eaddeffeca0d57ada1bbf75a0c34f3ae030d4cc
Shaila Zaman <shaila.zaman96@gmail.com>
2021-09-13 16:06:03 -0500

    generating daywise model data

---
+++ scripts/8.dc-generate-model-data.R
@@ -0,0 +1,41 @@
+#-------------------------#
+#--------LIBRARIES--------#
+#-------------------------#
+library(dplyr)
+# library(zoo)
+
+
+
+
+#-------------------------#
+#-----GLOBAL VARIABLES----#
+#-------------------------#
+# script_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+# project_dir <- dirname(script_dir)
+# setwd(project_dir)
+# 
+# source(file.path(script_dir, 'us-common-functions.R'))
+
+
+
+
+#-------------------------#
+#---FUNCTION DEFINITION---#
+#-------------------------#
+generate_daywise_model_data <- function() {
+  physiological_data_path <- file.path(project_dir, curated_data_dir, physiological_data_dir)
+  
+  
+  
+}
+
+
+
+
+#-------------------------#
+#-------Main Program------#
+#-------------------------#
+# generate_daywise_model_data()
+
+
+
